bac10b68bbd7150feaa071c577fa1345eb5d3455
tpemartin <tpemartin@gmail.com>
2021-11-24 16:23:31 +0800

    Create w10-graph01.R

---
+++ temp/w10-graph01.R
@@ -0,0 +1,42 @@
+###########################################
+#  01  平日較多夜間停留人數的區域           #
+###########################################
+
+library(readr)
+library(tidyverse)
+
+demo <- read_csv("temp/2020Nov_district.csv",locale = locale(encoding = "BIG5"))
+
+subset1 <- demo %>%
+  filter(NIGHT_WORK >= NIGHT_WEEKEND) %>% #View()
+  select(COUNTY, TOWN) %>%
+  mutate(
+    COUNTRY= as.factor(COUNTY)
+  ) 
+subset2 <- subset1 %>%
+  group_by(COUNTY) %>%
+  summarise(
+    count=n()
+  )
+
+# subset1$COUNTY <- as.factor(subset1$COUNTY)
+# levels(subset1$COUNTY)
+plot1 <- ggplot(
+  data = subset1,
+  aes( y = COUNTY))+
+  geom_bar( fill = "cyan3",
+            width = 0.5 #input$width
+            )+
+  geom_text(stat = "count",
+            aes(label = (..count..)),
+            vjust = -0.1, #input$vjust
+            hjust = 0.1, #input$hjust
+            size = 4 #input$size
+    )+ 
+  labs(title = "2020年11月各縣市平日夜間停留人數較多的區域數量",
+    subtitle="")+
+  scale_x_(
+    expand=
+  )
+
+plot1
